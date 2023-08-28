c653bd1190a9f25737668ef505d7b6f6cc923e05
ain <96311033+nurulainsalleh@users.noreply.github.com>
2023-08-29 02:23:41 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<link rel="icon" type="images/logo.png" href="image/icon5.png">
+</html>
+<?php include('allhead.php'); ?>
+
+<!-- Page Content -->
+<div class="container" style="width: 1000px;">
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<center><img src="images/emark.gif" height="70%"></center>
+
+
+	<style type="text/css">
+            body{
+                  width: 100%;
+                  background: url(images/test.gif) ;
+                  background-position: center center;
+                  background-repeat: no-repeat;
+                  background-attachment: fixed;
+                  background-size: cover;
+                }
+          </style>
+	</head>
+	
+
+
+	<hr>
+
+
+	<?php include('allfoot.php'); ?>
